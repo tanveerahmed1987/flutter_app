81e0b88c4b58144d95fb424d112987e4aff3c302
Tanveer Ahmed <tanveer.ahmed.contractor@move.com>
2020-10-09 01:17:20 +0530

    Row and Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,50 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Tutorial',
-      home: MyHomeWidget(),
+      home: Home(),
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.lime,
       ),
       debugShowCheckedModeBanner: false,
     )
   );
 }
 
-class MyHomeWidget extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar( title: Text('MyHomeWidget')),
-      body: Center(child: Text('Hello MyHomeWidget!', style: TextStyle( color: Colors.black, fontSize: 20.0))),
-      floatingActionButton: FloatingActionButton( 
-          elevation: 10.0, 
-          child: Icon(Icons.add), 
-          onPressed: (){ } 
+      body: Center(
+        child: Row(children: [
+          Name('Cat'),
+          SizedBox(width: 9.0,),
+          Name('Dog'),
+          SizedBox(width: 9.0,),
+          Name('Elephant'),
+        ],
+      )
+      )
+    );
+  }
+}
+
+class Name extends StatelessWidget {
+
+  final String _name;
+
+  const Name(this._name);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.orange), 
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(this._name, 
+          style: TextStyle(fontSize: 30.0),
+        ),
       ),
-      drawer: Drawer( 
-        child: ListView( 
-          children: const <Widget>[ 
-            DrawerHeader( 
-              decoration: BoxDecoration(color: Colors.green), 
-              child: Text( 'Drawer Header',style: TextStyle(color: Colors.white,fontSize: 24)), 
-            ), 
-            ListTile(title: Text('Item 1'),leading : Icon(Icons.people)), 
-            ListTile(title: Text('Item 2'), leading: Icon(Icons.mail)), 
-      ],),), 
-      bottomNavigationBar : BottomNavigationBar( 
-        currentIndex : 0, fixedColor: Colors.green, 
-        items: [ BottomNavigationBarItem(title : Text("Home"), icon : Icon(Icons.home)), 
-                 BottomNavigationBarItem(title : Text("Search"), icon : Icon(Icons.search)), 
-                 BottomNavigationBarItem(title : Text("Profile"), icon : Icon(Icons.account_circle))], 
-        onTap: (int indexOfItem){ }
-      ),
-      backgroundColor: Colors.greenAccent,
     );
   }
 }
