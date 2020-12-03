07981508005284473f702c0119d96e2eaf0ff879
Tanveer Ahmed <tanveer.ahmed.contractor@move.com>
2020-12-04 00:54:07 +0530

    Button Types

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,9 +8,79 @@ class Home extends StatelessWidget {
   Widget build(BuildContext buildContext) {
     return Scaffold(      
       appBar: MyAppBar(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+        onPressed: () { 
+          print('FloatingActionButton pressed');
+        },
+      ),
       body: Container(
         color: Colors.teal, 
-        child: Center(child: Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white))),
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {
+                        print('Flat Button is pressed');
+                      }, 
+                      child: Text('Button 1'), 
+                    ),
+                  ),
+
+                  Expanded(
+                    child: RaisedButton(
+                      onPressed: () {
+                          print('Raised Button is pressed');
+                      }, 
+                      child: Text('Button 2'), 
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.notifications), 
+                      onPressed: () {
+                        print('Icon Button Pressed');
+                      }
+                    ),
+                  ),
+
+                  Expanded(
+                    child: InkWell(
+                      child: Container(child: Text('Button 4')),
+                      onTap: () {
+                        print('InkWell button pressed');
+                      },
+                    ),
+                  )
+                ],
+              ),Row(
+                children: [
+                  Expanded(
+                    child: OutlineButton(
+                      onPressed: () {
+                        print('OutlineButton pressed');
+                      },
+                      
+                      textColor: Colors.red,
+                      child: Text('Button 5'),
+
+                    ),
+                  ),
+
+                
+                ],
+              ),
+            ],
+          ),
+        ) ,
       ),
     );
   } 
